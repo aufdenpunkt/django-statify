f8d8afd9ced28c0b47374434d9a486a7a418ff73
Christian Schweinhardt <are.u.kidding@me.com>
2013-04-17 12:19:29 +0200

    Some updates

---
+++ setup.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+#
+
+# Core imports
+from setuptools import setup
+
+
+dependency_links = [
+    'https://github.com/christian-schweinhardt/django-medusa.git#egg=django_medusa'
+]
+
+
+install_requires = [
+    'futures==2.1.3',
+    'requests==1.1.0',
+    'paramiko==1.10.0',
+    'scpclient==0.4',
+    'pycrypto==2.6'
+
+]
+
+
+setup(
+    name='django-statify',
+    description='Build out a static version of your website and deploy it.',
+    version='0.1.1',
+    author='Christian Schweinhardt',
+    author_email='are.u.kidding@me.com',
+    url='https://github.com/christian-schweinhardt/django-statify',
+    download_url='https://github.com/christian-schweinhardt/django-statify.git',
+    package_dir={'statify': 'statify'},
+    packages=['statify'],
+    zip_safe=False,
+    install_requires=install_requires,
+    dependency_links = dependency_links,
+    include_package_data=True,
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'Operating System :: OS Independent',
+        'Topic :: Software Development'
+    ],
+)
