5141b1b8f7e7bcb746a0110f6a6076b43d389f44
Christian Schweinhardt <are.u.kidding@me.com>
2020-08-20 12:31:09 +0200

    Update settings.py

--- statify/settings.py
+++ statify/settings.py
@@ -11,7 +11,7 @@ from django.conf import settings
 STATIFY_BUILD_SETTINGS = getattr(settings, 'STATIFY_BUILD_SETTINGS', '')
 STATIFY_USE_CMS = getattr(settings, 'STATIFY_USE_CMS', False)
 STATIFY_IGNORE_MEDIA = getattr(settings, 'STATIFY_IGNORE_MEDIA', False)
-STATIFY_IGNORE_STATIC = getattr(settings, 'STATIFY_IGNORE_MEDIA', False)
+STATIFY_IGNORE_STATIC = getattr(settings, 'STATIFY_IGNORE_STATIC', False)
 STATIFY_PROJECT_DIR = getattr(settings, 'STATIFY_PROJECT_DIR', os.path.join(os.path.dirname(os.path.abspath(__file__)), '../'))
 STATIFY_UPLOAD_PATH = getattr(settings, 'STATIFY_UPLOAD_PATH', 'statify/releases/')
 STATIFY_EXCLUDED_STATIC = getattr(settings, 'STATIFY_EXCLUDED_STATIC', ['admin', 'statify', 'tmp', 'root'])
